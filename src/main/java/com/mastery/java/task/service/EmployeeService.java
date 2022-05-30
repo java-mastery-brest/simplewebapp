2d84aa4e6d1c1659e8303fe478dd83ce716e1239
adam-pietrasik <adam.pietrasik90@gmail.com>
2022-05-30 17:44:38 +0200

    EmployeeService connection to EmployeeDao

--- src/main/java/com/mastery/java/task/service/EmployeeService.java
+++ src/main/java/com/mastery/java/task/service/EmployeeService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
@@ -18,8 +19,23 @@ public class EmployeeService {
     }
 
     public List<Employee> getEmployees(){
-        //TODO: return employees list from employeeDao
         return employeeDao.getEmployees();
+    }
+
+    public Optional<Employee> getEmployee(Long id){
+        return employeeDao.getEmployee(id);
+    }
+
+    public void deleteEmployee(Long id){
+        employeeDao.deleteEmployee(id);
+    }
+
+    public void updateEmployee(Employee employee, Long id){
+        employeeDao.updateEmployee(employee, id);
+    }
+
+    public void insertEmployee(Employee employee){
+        employeeDao.insertEmployee(employee);
     }
 
 }
